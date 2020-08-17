6634fb3a2239e6e6d3d8fe4fc1630f6d4e120685
5abhisheksaxena <5abhisheksaxena@gmail.com>
2020-08-17 20:43:22 +0530

    Implement Builder pattern to create custom toast

--- toaster/src/main/java/in/abhisheksaxena/toaster/Toaster.kt
+++ toaster/src/main/java/in/abhisheksaxena/toaster/Toaster.kt
@@ -2,6 +2,7 @@ package `in`.abhisheksaxena.toaster
 
 import android.content.Context
 import android.view.LayoutInflater
+import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
@@ -13,8 +14,17 @@ import androidx.constraintlayout.widget.ConstraintLayout
  * @since 17-08-2020 06:25
  */
 
-object Toaster {
+class Toaster private constructor(
+    private val context: Context,
+    private var message: CharSequence,
+    private var drawableRes: Int?
+) {
 
+
+    companion object {
+        private var messageTextView: TextView? = null
+        private var leftDrawable: ImageView? = null
+        private lateinit var toast: Toast
         const val LENGTH_SHORT = Toast.LENGTH_SHORT
         const val LENGTH_LONG = Toast.LENGTH_LONG
 
@@ -24,23 +34,56 @@ object Toaster {
             drawableRes: Int? = null,
             duration: Int = LENGTH_SHORT
         ): Toast {
+            val rootView = initView(context)
+            prepare(message, drawableRes)
+            toast.duration = duration
+            toast.view = rootView
+            return toast
+        }
 
-            val toast = Toast(context)
+        fun pop(toaster: Toaster): Toast {
+            val rootView = initView(toaster.context)
+            prepare(toaster.message, toaster.drawableRes)
+            toast.view = rootView
+            return toast
+        }
+
+        private fun initView(context: Context): View {
+            toast = Toast(context)
 
             val inflater =
                 context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
             val rootView = inflater.inflate(R.layout.layout_toast, ConstraintLayout(context), false)
 
-            val messageTextView = rootView.findViewById<TextView>(R.id.message_text_view)
-            messageTextView.text = message
+            messageTextView = rootView.findViewById(R.id.message_text_view)
+            leftDrawable = rootView.findViewById(R.id.left_drawable_image_view)
 
-            val leftDrawable = rootView.findViewById<ImageView>(R.id.left_drawable_image_view)
-            drawableRes?.let {
-                leftDrawable.setImageResource(it)
-            }
-
-            toast.duration = duration
-            toast.view = rootView
-            return toast
+            return rootView
         }
+
+        private fun prepare(message: CharSequence, drawableRes: Int?) {
+            messageTextView?.text = message
+            drawableRes?.let {
+                leftDrawable?.setImageResource(it)
+            }
+        }
+    }
+
+    class Builder(private val context: Context) {
+
+        private var message: CharSequence = ""
+        private var leftDrawableRes: Int? = null
+
+        fun setMessage(message: CharSequence) = apply {
+            this.message = message
+        }
+
+        fun setLeftDrawable(leftDrawableRes: Int) = apply {
+            this.leftDrawableRes = leftDrawableRes
+        }
+
+        fun make(): Toaster {
+            return Toaster(context, message, leftDrawableRes)
+        }
+    }
 }
