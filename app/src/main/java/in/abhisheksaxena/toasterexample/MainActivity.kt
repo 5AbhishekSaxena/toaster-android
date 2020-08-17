9a59144cdd5011baa00d2854895103e5ba70ca6d
5abhisheksaxena <5abhisheksaxena@gmail.com>
2020-08-17 10:40:45 +0530

    Create an example app.

--- app/src/main/java/in/abhisheksaxena/toasterexample/MainActivity.kt
+++ app/src/main/java/in/abhisheksaxena/toasterexample/MainActivity.kt
@@ -1,11 +1,26 @@
 package `in`.abhisheksaxena.toasterexample
 
+import `in`.abhisheksaxena.toaster.Toaster
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+import android.R.id.message
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val message = "Some message"
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+        button.setOnClickListener {
+            Toaster.pop(
+                this,
+                message,
+                R.drawable.ic_baseline_all_inclusive_24
+            ).show()
+        }
     }
 }
