87c5d9b66ee157ac849be2a70d86703c179bac00
5abhisheksaxena <5abhisheksaxena@gmail.com>
2020-08-17 10:58:26 +0530

    Minor changes

--- app/src/main/java/in/abhisheksaxena/toasterexample/MainActivity.kt
+++ app/src/main/java/in/abhisheksaxena/toasterexample/MainActivity.kt
@@ -6,8 +6,6 @@ import android.os.Bundle
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
-import android.R.id.message
-
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
